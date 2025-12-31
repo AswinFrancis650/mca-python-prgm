f4e0bb9af036f432f9a36619a4f67a39740f8004
Aswin Francis <aswinkf@gmail.com>
2025-12-31 16:42:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from Graphics import rectangle, circle
-from Graphics.ThreeDgraphics import cuboid, sphere
-# Rectangle
-length = int(input("Enter length of rectangle: "))
-breadth = int(input("Enter breadth of rectangle: "))
-print("Rectangle Area:", rectangle.area(length, breadth))
-print("Rectangle Perimeter:", rectangle.perimeter(length, breadth))
-# Circle
-radius = int(input("\nEnter radius of circle: "))
-print("Circle Area:", circle.area(radius))
-print("Circle Perimeter:", circle.perimeter(radius))
-# Cuboid
-length = int(input("\nEnter length of cuboid: "))
-breadth = int(input("Enter breadth of cuboid: "))
-height = int(input("Enter height of cuboid: "))
-print("Cuboid Surface Area:", cuboid.Surface_area(length, breadth, height))
-print("Cuboid Volume:", cuboid.Volume(length, breadth, height))
-# Sphere
-radius = int(input("\nEnter radius of sphere: "))
-print("Sphere Surface Area:", sphere.surface_area(radius))
-print("Sphere Volume:", sphere.volume(radius))
+from arithmetic import operations
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+print("Sum=",operations.add(a,b))
+print("Difference=",operations.sub(a,b))
+print("Product=",operations.mult(a,b))
+print("Quotient=",operations.div(a,b))
